e7734a0e313a060cf54cb2d65df265cebcbccddb
LJH-3410 <sos341000@gmail.com>
2023-06-13 10:17:03 +0900

    테스트

---
+++ project_ex9/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Movenet from './Movenet';
+
+function App() {
+  return (
+    <div>
+      <h1>Movenet Example</h1>
+      <p>This is an example of how to use Movenet with React.</p>
+      <Movenet />
+    </div>
+  );
+}
+
+export default App;
